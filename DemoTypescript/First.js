d2d54aa84b0a3e0410ef23f7b552e3722b11559a
Iamneo <techadmin@iamneo.ai>
2023-11-08 05:12:27 +0000

    Backup Commit

--- DemoTypescript/First.js
+++ DemoTypescript/First.js
@@ -3,3 +3,16 @@ var firstname = 'Tom';
 var age = 33;
 console.log("Name =" + firstname);
 console.log("Age =" + age);
+var fno = 70;
+var sno = 80;
+var csno = '40';
+var result = fno + sno + csno;
+var result1 = fno + csno + sno;
+var result2 = csno + sno + fno;
+console.log(result);
+console.log(result1);
+console.log(result2);
+var fruits = ['Apple', 'Mango', 'Orange'];
+for (var i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
--- DemoTypescript/First.ts
+++ DemoTypescript/First.ts
@@ -5,3 +5,23 @@ var age:number =33;
 
 console.log("Name ="+firstname);
 console.log("Age ="+ age);
+
+var fno:number=70;
+var sno:number=80;
+var csno:string='40';
+
+var result = fno + sno +csno
+var result1 = fno + csno +sno
+var result2 = csno + sno + fno
+console.log(result)
+console.log(result1)
+console.log(result2)
+
+var fruits : string[] = ['Apple','Mango','Orange']
+for(var i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i])
+}
+function sayHello():void{
+    console.log('Hello World')
+}
