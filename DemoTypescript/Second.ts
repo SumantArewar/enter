839cc40fb15073ba6aff1d2e2f7035defc15dc42
Iamneo <techadmin@iamneo.ai>
2023-11-09 04:08:36 +0000

    Backup Commit

---
+++ DemoTypescript/Second.ts
@@ -0,0 +1,21 @@
+class Person 
+{
+    private name:string
+    private age:number
+    public constructor(name:string,age:number)
+    {
+        this.name=name;
+        this.age=age;
+    }
+    public getName():string
+    {
+        return this.name
+    }
+    public getAge():number
+    {
+        return this.age
+    }
+}
+var person = new Person('Tom',22)
+console.log(person.getName())
+console.log(person.getAge())
---
+++ demoang/src/app/employee.ts
@@ -0,0 +1,14 @@
+export class Employee 
+{
+    id:number 
+    name:string
+    salary:number
+    city:string
+    constructor(id:number,name:string,salary:number,city:string)
+    {
+        this.id=id
+        this.name=name
+        this.salary=salary
+        this.city=city
+    }
+}
