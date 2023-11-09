af6765f4b91b9517448e7e03a9a358eaa480b51a
Iamneo <techadmin@iamneo.ai>
2023-11-09 07:08:36 +0000

    Backup Commit

--- demoang/src/app/demo/demo.component.ts
+++ demoang/src/app/demo/demo.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
+  name:string = 'Tom'
+  age:number = 33
+
   constructor() { }
 
   ngOnInit() {
