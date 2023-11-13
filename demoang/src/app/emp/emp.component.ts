137d4550501b1c89eba1fe7c00afe04f84a50c85
Iamneo <techadmin@iamneo.ai>
2023-11-13 09:23:26 +0000

    Backup Commit

---
+++ demoang/src/app/emp/emp.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-emp',
+  templateUrl: './emp.component.html',
+  styleUrls: ['./emp.component.css']
+})
+export class EmpComponent implements OnInit {
+
+  constructor(private formbuilder:FormBuilder) { }
+  empform = this.formbuilder.group({
+    firstname:[''],
+    age:[''],
+    address:this.formbuilder.group({
+      line:[''],
+      city:[''],
+      state:['']
+    })
+  })
+
+  ngOnInit() {
+  }
+
+}
